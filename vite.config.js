9ad852c6ab588524c792799e4329e684feba507d
a1000rhan <1000rhan@gmail.com>
2022-08-29 01:33:34 +0300

    config server

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://127.0.0.1:8080/",
+  base: "http://0.0.0.0:8080/",
   server: {
     origin: "http://0.0.0.0:8080",
     host: "0.0.0.0",
