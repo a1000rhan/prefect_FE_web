a08f97a07b8d50af0993ace664fbcd6d801c5a91
a1000rhan <1000rhan@gmail.com>
2022-09-02 23:06:11 +0300

    port

--- src/store/api.jsx
+++ src/store/api.jsx
@@ -4,6 +4,7 @@ const api = axios.create({
   // baseURL: "http://localhost:3000/",
   //   baseURL: "http://172.20.10.3:8000/",
   baseURL: "http://159.203.190.94/", //live
+  // baseURL: "http://10.116.0.3/", //live
 });
 
 export default api;
