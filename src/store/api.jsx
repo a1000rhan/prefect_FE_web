a8b7634cf8209f2d86b298eb14f7d9468f5b037c
a1000rhan <1000rhan@gmail.com>
2022-09-22 19:12:21 +0300

    a

--- src/store/api.jsx
+++ src/store/api.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:3030/api",
   //   baseURL: "http://172.20.10.3:8000/",
-  baseURL: "http://159.203.190.94/api", //live
-  // baseURL: "http://10.116.0.3/", //live
+  // baseURL: "http://159.203.190.94/api", //live
+  baseURL: "http://api.perfectq8.com/api", //live
 });
 
 export default api;
