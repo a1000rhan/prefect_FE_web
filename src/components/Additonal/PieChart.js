eef510a4101794d92a1a5857df2d1af2d1d20eb0
a1000rhan <1000rhan@gmail.com>
2022-10-10 22:24:02 +0300

    api

---
+++ src/components/Additonal/PieChart.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+const PieChart = ({ number }) => {
+  const [state, setState] = useState({
+    optionsRadial: {
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 225,
+          hollow: {
+            margin: 0,
+            size: "70%",
+            background: "#fff",
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: "front",
+            dropShadow: {
+              enabled: true,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24,
+            },
+          },
+          track: {
+            background: "#fff",
+            strokeWidth: "67%",
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35,
+            },
+          },
+
+          dataLabels: {
+            showOn: "always",
+            name: {
+              offsetY: -20,
+              show: true,
+              color: "#888",
+              fontSize: "13px",
+            },
+            value: {
+              formatter: function (val) {
+                return val;
+              },
+              color: "#111",
+              fontSize: "30px",
+              show: true,
+            },
+          },
+        },
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 0.6,
+          gradientToColors: ["#ABE5A1"],
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100],
+        },
+      },
+      stroke: {
+        lineCap: "round",
+      },
+      labels: ["Number"],
+    },
+    seriesRadial: [number],
+  });
+  return (
+    <div>
+      <div className="row">
+        <div className="col radial-chart">
+          <Chart
+            options={state.optionsRadial}
+            series={state.seriesRadial}
+            type="radialBar"
+            width="280"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
--- src/store/api.jsx
+++ src/store/api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3030/api",
+  // baseURL: "http://localhost:3030/api",
   //   baseURL: "http://172.20.10.3:8000/",
   // baseURL: "http://159.203.190.94/api", //live
-  // baseURL: "https://api.perfectq8.com/api", //live
+  baseURL: "https://api.perfectq8.com/api", //live
 });
 
 export default api;
